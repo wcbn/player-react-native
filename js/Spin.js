8ca691d2790f0459b19e6e885ea713bd4abb44d7
dctalbot <dctalbot@umich.edu>
2019-01-21 20:22:57 -0500

    staggered animation

--- js/Spin.js
+++ js/Spin.js
@@ -2,11 +2,21 @@ import React from 'react'
 import { StyleSheet, Text, View, Animated } from 'react-native'
 import { windowStyles } from './styles/components'
 
+const arr = []
+for (var i = 0; i < 12; i++) {
+  arr.push(i)
+}
+
 export default class Spin extends React.Component {
   constructor(props) {
     super(props)
+    this.animatedValue = []
+    arr.forEach(value => {
+      this.animatedValue[value] = new Animated.Value(0)
+    })
+
     this.state = {
-      opacities: new Array(this.props.lines).fill(new Animated.Value(0)),
+      // opacities: new Array(this.props.lines).fill(new Animated.Value(0)),
       x: 0,
       y: 0
     }
@@ -17,52 +27,45 @@ export default class Spin extends React.Component {
   }
 
   animate() {
-    this.state.opacities.forEach((elem, i) => {
-      Animated.loop(
+    const animations = arr.map(item => {
+      return Animated.loop(
         Animated.sequence([
-          Animated.timing(this.state.opacities[i], {
-            toValue: 1
-            // delay: i * 100 // / this.props.lines / 20
+          Animated.timing(this.animatedValue[item], {
+            toValue: 1,
+            duration: 500
           }),
-          Animated.timing(this.state.opacities[i], {
-            toValue: 0
-            // delay: i * 100 // / this.props.lines / 20
+          Animated.timing(this.animatedValue[item], {
+            toValue: 0,
+            duration: 500
           })
         ])
-      ).start()
+      )
     })
+    Animated.stagger(100, animations).start()
   }
 
   render() {
-    const blocks = Array(this.props.lines)
-      .fill()
-      .map((_, i) => {
-        const angle = (~~((360 / this.props.lines) * i) * Math.PI) / 180 //+ opts.rotate
-
-        return (
-          <Animated.View
-            key={i}
-            style={{
-              backgroundColor: 'yellow',
-              borderRadius: '50%',
-              position: 'absolute',
-              height: this.props.width,
-              width: this.props.width,
-              transform: [
-                { translateX: this.props.radius * Math.sin(angle) },
-                { translateY: this.props.radius * Math.cos(angle) }
-              ],
-              opacity: this.state.opacities[i]
-            }}
-          />
-        )
-      })
-
-    return (
-      <View style={styles.container}>
-        {blocks}
-      </View>
-    )
+    const animations = arr.map((a, i) => {
+      const angle = (~~((360 / 12) * i) * Math.PI) / 180 //+ opts.rotate
+      return (
+        <Animated.View
+          key={i}
+          style={{
+            backgroundColor: 'yellow',
+            borderRadius: '50%',
+            position: 'absolute',
+            height: this.props.width,
+            width: this.props.width,
+            transform: [
+              { translateX: this.props.radius * Math.sin(angle) },
+              { translateY: this.props.radius * Math.cos(angle) }
+            ],
+            opacity: this.animatedValue[a]
+          }}
+        />
+      )
+    })
+    return <View style={styles.container}>{animations}</View>
   }
 }
 
