06e8c4e359f9a1eec68245346078f68ad221a926
dctalbot <dctalbot@umich.edu>
2019-01-26 02:46:53 -0500

    class -> functional componenet

--- js/components/ShowListing.js
+++ js/components/ShowListing.js
@@ -1,25 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import { windowStyles, listStyles } from '../styles/components'
+import { Text, View } from 'react-native'
+import { listStyles } from '../styles/components'
 import { colors } from '../styles/main'
 
-
-export default class ShowListing extends React.PureComponent {
-  render() {
-    return (
-      <View style={windowStyles.container}>
-        <View style={{ ...listStyles.item, ...styles.listItem }}>
-          <Text style={{color: colors.inactive}}>{this.props.data.name}</Text>
-        </View>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  listItem: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-})
+export default (ShowListing = props => (
+  <View style={listStyles.item}>
+    <Text style={{ color: colors.inactive }}>{props.data.name}</Text>
+  </View>
+))
