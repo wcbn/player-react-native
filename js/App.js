92eca7f5edb982a2b090929e1e6dc6493d0fafb1
dctalbot <dctalbot@umich.edu>
2019-01-26 02:20:08 -0500

    make playlist default page

--- js/App.js
+++ js/App.js
@@ -66,7 +66,7 @@ const AppNavigator = createBottomTabNavigator(
       inactiveBackgroundColor: colors.primary,
       activeBackgroundColor: colors.highlight
     },
-    initialRouteName: 'Radio'
+    initialRouteName: 'Playlist'
   }
 )
 
