0242847c9a1deca0a676d9a31302f4c2211d1523
dctalbot <dctalbot@umich.edu>
2019-05-12 22:35:20 -0400

    functional components

--- js/components/settings/RequestLine.js
+++ js/components/settings/RequestLine.js
@@ -3,27 +3,17 @@ import { StyleSheet, Text, View, Linking, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { colors } from '../../styles/main'
 
-export default class RequestLine extends React.PureComponent {
-  render() {
-    return (
-      <TouchableOpacity
-        style={styles.container}
-        onPress={() => Linking.openURL(`tel:17347633500`)}
-      >
-        <Icon
-          name={'md-call'}
-          size={30}
-          color={colors.active}
-        />
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>
-            Studio Request Line{'\n'}(734) 763-3500
-          </Text>
-        </View>
-      </TouchableOpacity>
-    )
-  }
-}
+export default (Review = props => (
+  <TouchableOpacity
+    style={styles.container}
+    onPress={() => Linking.openURL(`tel:17347633500`)}
+  >
+    <Icon name={'md-call'} size={30} color={colors.active} />
+    <View style={styles.textContainer}>
+      <Text style={styles.text}>Studio Request Line{'\n'}(734) 763-3500</Text>
+    </View>
+  </TouchableOpacity>
+))
 
 const styles = StyleSheet.create({
   container: {
