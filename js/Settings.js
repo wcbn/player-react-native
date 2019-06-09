c83e4c2cd41312f8b22f39103edf69c143b76787
dctalbot <dctalbot@umich.edu>
2019-06-09 02:42:06 -0400

    spit and polish

--- js/Settings.js
+++ js/Settings.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { StyleSheet, Text, View, Switch, AsyncStorage } from 'react-native'
-import SegmentedControlTab from 'react-native-segmented-control-tab'
 import { windowStyles, headerStyles } from './styles/components'
 import { colors } from './styles/main'
 import Separator from './components/Separator'
 import Share from './components/settings/ShareLink'
-import SmsExpo from './components/settings/SmsExpo'
 import ExternalLink from './components/settings/ExternalLink'
+import StreamSelection from './components/settings/StreamSelection'
+import Colophon from './components/settings/Colophon'
 
-const STREAMS = [
-  'http://floyd.wcbn.org:8000/wcbn-mid.mp3',
-  'http://floyd.wcbn.org:8000/wcbn-hi.mp3',
-  'http://floyd.wcbn.org:8000/wcbn-hd.mp3'
-]
+const GOOGLE_HANGOUTS_URL =
+  'https://hangouts.google.com/chat/person/118357885959401668528'
+const DONATION_URL =
+  'https://leadersandbest.umich.edu/find/#!/give/basket/fund/361991'
 
 export default class Settings extends React.Component {
   static navigationOptions = {
@@ -20,88 +19,20 @@ export default class Settings extends React.Component {
     ...headerStyles
   }
 
-  constructor() {
-    super()
-    this.state = {
-      selectedStreamIndex: null // fetch ASAP in async componentDidMount ¯\_(ツ)_/¯
-    }
-  }
-
-  async componentDidMount() {
-    const streamUrl = (await AsyncStorage.getItem('STREAM_URL')) || STREAMS[2]
-
-    this.setState({
-      selectedStreamIndex: STREAMS.indexOf(streamUrl)
-    })
-  }
-
-  setStreamSetting = index => {
-    AsyncStorage.setItem('STREAM_URL', STREAMS[index])
-    this.setState({ selectedStreamIndex: index })
-    // AsyncStorage.getItem('STREAM_URL').then(resp => console.log(resp))
-  }
-
-  renderStreamOption() {
-    return (
-      <View style={styles.option}>
-        <Text style={styles.optionLabel}>Stream Quality</Text>
-        <SegmentedControlTab
-          values={['Low', 'Medium', 'High']}
-          thumbColor={colors.active}
-          selectedIndex={this.state.selectedStreamIndex}
-          onTabPress={this.setStreamSetting}
-          activeTabStyle={{ backgroundColor: colors.highlight }}
-          tabStyle={{
-            backgroundColor: colors.primary,
-            borderColor: colors.active
-          }}
-          activeTabTextStyle={{ color: colors.active }}
-          tabTextStyle={{ color: colors.inactive }}
-        />
-        <View style={styles.streamInfoView}>
-          <Text style={styles.streamInfoText}>64 kbps</Text>
-          <Text style={styles.streamInfoText}>128 kbps</Text>
-          <Text style={styles.streamInfoText}>320 kbps</Text>
-        </View>
-        <View style={styles.streamInfoView}>
-          <Text style={styles.streamInfoText}>
-            If you're experiencing buffering, try a lower data rate.
-          </Text>
-        </View>
-      </View>
-    )
-  }
-
-  renderThemeOption() {
-    return (
-      <View style={styles.option}>
-        <Text style={styles.optionLabel}>Dark Mode</Text>
-        <Switch
-          value={this.state.darkMode}
-          disabled={true}
-          trackColor={{ true: colors.active }}
-          onValueChange={() => {
-            const newVal = !this.state.darkMode
-            this.setState({
-              darkMode: newVal
-            })
-            AsyncStorage.setItem('DARK_MODE', JSON.stringify(newVal))
-          }}
-        />
-      </View>
-    )
-  }
-
   renderLinks() {
     return (
-      <View style={styles.LinkList}>
+      <View style={styles.LinksView}>
         <ExternalLink
           url={'tel:17347633500'}
           text={`Studio Request Line${'\n'}(734) 763-3500`}
           icon={'md-call'}
         />
         <Separator color={colors.active} />
-        <SmsExpo />
+        <ExternalLink
+          url={GOOGLE_HANGOUTS_URL}
+          text={`Message the DJ`}
+          icon={'md-text'}
+        />
         <Separator color={colors.active} />
         <ShareLink />
         <Separator color={colors.active} />
@@ -112,9 +43,7 @@ export default class Settings extends React.Component {
         />
         <Separator color={colors.active} /> */}
         <ExternalLink
-          url={
-            'https://leadersandbest.umich.edu/find/#!/give/basket/fund/361991'
-          }
+          url={DONATION_URL}
           text={'Give to WCBN'}
           icon={'md-cash'}
         />
@@ -126,8 +55,9 @@ export default class Settings extends React.Component {
     return (
       <View style={windowStyles.container}>
         <View style={styles.content}>
-          {this.renderStreamOption()}
+          <StreamSelection />
           {this.renderLinks()}
+          <Colophon />
         </View>
       </View>
     )
@@ -136,24 +66,10 @@ export default class Settings extends React.Component {
 
 const styles = StyleSheet.create({
   content: {
-    padding: 20
+    padding: 15,
+    paddingTop: 5
   },
-  option: {
-    marginBottom: 10
-  },
-  optionLabel: { color: colors.inactive, marginBottom: 4, fontSize: 16 },
-  streamInfoView: { flexDirection: 'row' },
-  streamInfoText: { flex: 1, padding: 4, color: colors.inactive },
-  LinkList: {
-    marginTop: 20
+  LinksView: {
+    marginTop: 10
   }
 })
-
-//componentdidmount
-// let darkMode = JSON.parse(await AsyncStorage.getItem('DARK_MODE'))
-
-// if (darkMode == null) {
-//   darkMode = true // DEFAULT TO TRUE TODO: FLIP ONCE LIGHT MODE IS IMPLEMENTED
-// }
-
-// https://leadersandbest.umich.edu/find/#!/give/basket/fund/361991
