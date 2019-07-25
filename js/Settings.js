d0db8878acee4c5f759c918e9a47321796d973dd
dctalbot <dctalbot@umich.edu>
2019-07-25 02:33:38 -0400

    themed

--- js/Settings.js
+++ js/Settings.js
@@ -1,18 +1,13 @@
 import React from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  Linking,
-  Share
-} from 'react-native'
+import { StyleSheet, View, ScrollView, Linking, Share } from 'react-native'
 import { windowStyles, headerStyles } from './styles/components'
-import { colors } from './styles/main'
 import Separator from './components/Separator'
+import Screen from './components/Screen'
 import Link from './components/settings/Link'
 import StreamSelection from './components/settings/StreamSelection'
 import Colophon from './components/settings/Colophon'
+import ThemeSelection from './components/settings/ThemeSelection'
+import { withTheme } from './styles/theming'
 
 const GOOGLE_HANGOUTS_URL =
   'https://hangouts.google.com/chat/person/118357885959401668528'
@@ -22,13 +17,14 @@ const DONATION_URL =
 
 const GOOGLE_PLAY_URL = 'https://play.google.com/store/apps/details?id=org.wcbn'
 
-export default class Settings extends React.PureComponent {
+class Settings extends React.PureComponent {
   static navigationOptions = {
     title: 'Settings',
     ...headerStyles
   }
 
   renderLinks() {
+    const theme = this.props.theme
     return (
       <View style={styles.LinksView}>
         <Link
@@ -36,13 +32,13 @@ export default class Settings extends React.PureComponent {
           text={`Studio Request Line${'\n'}(734) 763-3500`}
           icon={'md-call'}
         />
-        <Separator color={colors.active} />
+        <Separator color={theme.secondary} />
         <Link
           onPress={() => Linking.openURL(GOOGLE_HANGOUTS_URL)}
           text={'Message the DJ'}
           icon={'md-text'}
         />
-        <Separator color={colors.active} />
+        <Separator color={theme.secondary} />
         <Link
           onPress={() =>
             Share.share({
@@ -52,13 +48,13 @@ export default class Settings extends React.PureComponent {
           text={'Share on Social Media'}
           icon={'md-share'}
         />
-        <Separator color={colors.active} />
+        <Separator color={theme.secondary} />
         <Link
           onPress={() => Linking.openURL(GOOGLE_PLAY_URL)}
           text={'Write a Review!'}
           icon={'md-thumbs-up'}
         />
-        <Separator color={colors.active} />
+        <Separator color={theme.secondary} />
         <Link
           onPress={() => Linking.openURL(DONATION_URL)}
           text={'Give to WCBN'}
@@ -70,13 +66,16 @@ export default class Settings extends React.PureComponent {
 
   render() {
     return (
-      <View style={windowStyles.container}>
+      <Screen>
         <ScrollView style={styles.content}>
           <StreamSelection />
+          <ThemeSelection
+            toggleTheme={this.props.screenProps.handleThemeChange}
+          />
           {this.renderLinks()}
           <Colophon />
         </ScrollView>
-      </View>
+      </Screen>
     )
   }
 }
@@ -89,3 +88,5 @@ const styles = StyleSheet.create({
     marginTop: 10
   }
 })
+
+export default withTheme(Settings)
