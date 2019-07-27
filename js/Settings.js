a8b3bf733d18a40be081d73a6b51b5e78f3d3795
dctalbot <dctalbot@umich.edu>
2019-07-26 23:32:58 -0400

    audited

--- js/Settings.js
+++ js/Settings.js
@@ -2,23 +2,36 @@ import React from 'react'
 import { StyleSheet, ScrollView } from 'react-native'
 import Screen from './components/Screen'
 import StreamSelection from './components/settings/StreamSelection'
-import Colophon from './components/settings/Colophon'
 import LinksList from './components/settings/LinksList'
+import Colophon from './components/settings/Colophon'
+import { getDefaultNavigationOptions } from './util/navigation'
 
-export default (Settings = props => {
-  return (
-    <Screen>
-      <ScrollView style={styles.content}>
-        <StreamSelection />
-        <LinksList toggleTheme={props.screenProps.handleThemeChange} />
-        <Colophon />
-      </ScrollView>
-    </Screen>
-  )
-})
+export default class Settings extends React.PureComponent {
+  static navigationOptions = ({ navigation, screenProps }) => {
+    return {
+      title: 'Settings',
+      ...getDefaultNavigationOptions(screenProps.theme)
+    }
+  }
+
+  render() {
+    return (
+      <Screen>
+        <ScrollView contentContainerStyle={styles.container}>
+          <StreamSelection />
+          <LinksList
+            handleThemeChange={this.props.screenProps.handleThemeChange}
+          />
+          <Colophon />
+        </ScrollView>
+      </Screen>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-  content: {
-    padding: 15
+  container: {
+    padding: 15,
+    paddingTop: 0
   }
 })
