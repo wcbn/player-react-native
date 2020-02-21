1b9f38f4313d462708aa8a7322cd2fa48aa7bbca
DavidTalbot <dctalbot@umich.edu>
2020-02-21 15:38:34 -0600

    use config

--- js/components/PlaylistPoll.tsx
+++ js/components/PlaylistPoll.tsx
@@ -1,6 +1,7 @@
 import React, { ReactChildren } from 'react'
 import { connect } from 'react-redux'
 import { fetchPlaylist } from '../redux/actionCreators'
+import { POLL_INTERVAL } from '../config'
 
 const mapDispatchToProps = {
   fetchPlaylist
@@ -17,7 +18,7 @@ class PlaylistPoll extends React.Component<{ fetchPlaylist: Function }> {
     }
 
     pollPlaylist()
-    setInterval(pollPlaylist, 40000)
+    setInterval(pollPlaylist, POLL_INTERVAL)
   }
   render() {
     return this.props.children
