3d7d137bda0ee5d1e7c7404b5a0ad8d212c1474b
DavidTalbot <dctalbot@umich.edu>
2020-02-21 15:16:30 -0600

    convert to ts

--- js/util/datetime.js
+++
@@ -1,5 +0,0 @@
-import dayjs from 'dayjs'
-
-export const humanizeTime = datetime => dayjs(datetime).format('h:mm A')
-
-export const humanizeDate = datetime => dayjs(datetime).format('MMMM D, YYYY')
---
+++ js/util/datetime.ts
@@ -0,0 +1,5 @@
+import dayjs from 'dayjs'
+
+export const humanizeTime = (datetime: string) => dayjs(datetime).format('h:mm A')
+
+export const humanizeDate = (datetime: string) => dayjs(datetime).format('MMMM D, YYYY')
