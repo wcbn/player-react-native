3c431a6c615fa48c05312cd41a9e1a90bc3e9a29
DavidTalbot <dctalbot@umich.edu>
2020-02-22 02:28:00 -0600

    hooks, react-navigation v5

---
+++ js/screens/Show.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import Separator from '../components/Separator'
+import Screen from '../components/Screen'
+import { humanizeTime, humanizeDate } from '../util/datetime'
+import { DjScroll, ShowDescription, EpisodeList } from '../components/show'
+import { BASE_URL } from '../config'
+
+export default function Show({ route, navigation }) {
+  const [state, setState] = useState({
+    description: '',
+    djs: [],
+    episodes: []
+  })
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: route.params.title
+    })
+
+    fetch(BASE_URL + `${route.params.url}.json`)
+      .then(response => response.json())
+      .then(response => {
+        response.episodes.forEach(e => {
+          e.beginning = humanizeDate(e.beginning)
+
+          e.songs.forEach(song => {
+            song.at = humanizeTime(song.at)
+          })
+        })
+
+        setState({
+          description: response.description,
+          djs: response.djs,
+          episodes: response.episodes
+        })
+      })
+  }, [])
+
+  return (
+    <Screen>
+      <DjScroll djs={state.djs} navigation={navigation} />
+      <Separator />
+      <ShowDescription text={state.description} />
+      <EpisodeList episodes={state.episodes} navigation={navigation} />
+    </Screen>
+  )
+}
