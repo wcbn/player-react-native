4777836fc6c0eca41b3cb1efe43089004fc15cf2
DavidTalbot <dctalbot@umich.edu>
2020-02-22 02:09:58 -0600

    convert to hooks

---
+++ js/screens/Episode.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FlatList } from 'react-native'
+import Song from '../components/Song'
+import Separator from '../components/Separator'
+import ListHeader from '../components/ListHeader'
+import Banner from '../components/Banner'
+import Screen from '../components/Screen'
+import { useTheme } from '../styles/theming'
+
+// since the only way to get to this screen is from a Show,
+// all of the data is passed in as navigation props, saving us an expensive fetch()
+
+export default function Episode({ navigation, route }) {
+  const theme = useTheme()
+  return (
+    <Screen>
+      <Banner
+        text={'Host:'}
+        host={route.params.dj}
+        onPress={() =>
+          navigation.navigate('Profile', {
+            url: route.params.dj_url,
+            title: route.params.dj
+          })
+        }
+      />
+      <FlatList
+        data={route.params.songs}
+        renderItem={({ item }) => <Song data={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        ListHeaderComponent={<ListHeader text={route.params.date} />}
+        stickyHeaderIndices={[0]}
+        overScrollMode={'never'}
+        ItemSeparatorComponent={() => <Separator color={theme.muted} />}
+      />
+    </Screen>
+  )
+}
