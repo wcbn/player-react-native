875929ced2e604b5e1524224d471d3730cd18162
DavidTalbot <dctalbot@umich.edu>
2020-02-22 02:17:56 -0600

    optional color

--- js/components/Separator.tsx
+++ js/components/Separator.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, View } from 'react-native'
 import React from 'react'
 
-const Separator = (props: { color: string }) => (
+const Separator = (props: { color?: string }) => (
   <View
     style={{
       height: StyleSheet.hairlineWidth,
