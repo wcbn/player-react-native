47a02976e6d54f2edc829c02b0a59902a39bcefe
DavidTalbot <dctalbot@umich.edu>
2020-02-24 02:06:54 -0600

    convert to hooks

--- js/components/profile/DjBio.js
+++ js/components/profile/DjBio.js
@@ -1,55 +1,63 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, Image, Linking } from 'react-native'
 import HTML from 'react-native-render-html'
 import { dimensions, spacing } from '../../styles/main'
-import { useTheme } from '../../styles/theming'
+import { ThemeContext } from '../../styles/theming'
+import { BASE_URL } from '../../config'
 
 export default DjBio = props => {
-  const theme = useTheme()
+  const { theme } = useContext(ThemeContext)
 
-  renderers = {
+  const styles = StyleSheet.create({
+    listsPrefixesRenderers: {
+      marginRight: spacing.xs
+    },
+    baseFontStyle: {
+      color: theme.textColor
+    },
+    image: {
+      marginTop: spacing.md,
+      marginBottom: 2
+    }
+  })
+
+  const renderers = {
     img: (htmlAttribs, children, convertedCSSStyles, passProps) => (
       <Image
         style={[
-          this.styles.image,
+          styles.image,
           {
             height:
               (parseInt(htmlAttribs.height) * passProps.imagesMaxWidth) /
               parseInt(htmlAttribs.width)
           }
         ]}
-        source={{ uri: `https://app.wcbn.org${htmlAttribs.src}` }}
+        source={{ uri: `${BASE_URL}${htmlAttribs.src}` }}
         key={htmlAttribs.src}
       />
     )
   }
 
-  listsPrefixesRenderers = {
+  const listsPrefixesRenderers = {
     ul: (htmlAttribs, children, convertedCSSStyles, passProps) => (
-      <ThemedText
-        style={this.styles.listsPrefixesRenderers}
-        color={'secondary'}
-      >
+      <ThemedText style={styles.listsPrefixesRenderers} color={'secondary'}>
         •
       </ThemedText>
     ),
     ol: (htmlAttribs, children, convertedCSSStyles, passProps) => (
-      <ThemedText
-        style={this.styles.listsPrefixesRenderers}
-        color={'secondary'}
-      >
+      <ThemedText style={styles.listsPrefixesRenderers} color={'secondary'}>
         {`${passProps.index + 1})`}
       </ThemedText>
     )
   }
 
-  // IMPORTANT NOTE : Do NOT use the StyleSheet API to create the styles you're going 
-  // to feed to tagsStyle and classesStyles. Although it might look like it's working 
-  // at first, the caching logic of react-native makes it impossible for this module 
-  // to deep check each of your style to properly apply the precedence and priorities 
+  // IMPORTANT NOTE : Do NOT use the StyleSheet API to create the styles you're going
+  // to feed to tagsStyle and classesStyles. Although it might look like it's working
+  // at first, the caching logic of react-native makes it impossible for this module
+  // to deep check each of your style to properly apply the precedence and priorities
   // of your nested tags' styles.
 
-  tagsStyles = {
+  const tagsStyles = {
     figure: {
       marginBottom: spacing.md
     },
@@ -64,27 +72,14 @@ export default DjBio = props => {
     }
   }
 
-  styles = StyleSheet.create({
-    listsPrefixesRenderers: {
-      marginRight: spacing.xs
-    },
-    baseFontStyle: {
-      color: theme.textColor
-    },
-    image: {
-      marginTop: spacing.md,
-      marginBottom: 2
-    }
-  })
-
   return (
     !!props.about && (
       <HTML
         html={props.about}
-        baseFontStyle={this.styles.baseFontStyle}
-        renderers={this.renderers}
-        listsPrefixesRenderers={this.listsPrefixesRenderers}
-        tagsStyles={this.tagsStyles}
+        baseFontStyle={styles.baseFontStyle}
+        renderers={renderers}
+        listsPrefixesRenderers={listsPrefixesRenderers}
+        tagsStyles={tagsStyles}
         imagesMaxWidth={dimensions.fullWidth}
         onLinkPress={(event, href) => {
           Linking.openURL(href)
