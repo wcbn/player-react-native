764e0fc7603e07603fc79b48e783b33e5cd57b54
DavidTalbot <dctalbot@umich.edu>
2020-02-24 02:06:18 -0600

    custom types

---
+++ js/types.ts
@@ -0,0 +1,17 @@
+export interface Song {
+  at?: string
+  artist?: string
+  name: string
+  request?: boolean
+  album?: string
+  label?: string
+  year?: number
+}
+
+export const defaultSong = {
+  name: '',
+  artist: '',
+  album: '',
+  label: '',
+  year: ''
+}
