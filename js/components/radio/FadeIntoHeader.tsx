bd894cc085a30ec718596d252454d2e7122f2bfa
DavidTalbot <dctalbot@umich.edu>
2020-02-24 02:07:07 -0600

    convert to ts

---
+++ js/components/radio/FadeIntoHeader.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import { StyleSheet } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import { dimensions } from '../../styles/main'
+import { ThemeContext } from '../../styles/theming'
+
+export default function FadeIntoHeader() {
+  const { theme } = useContext(ThemeContext)
+  return (
+    <LinearGradient colors={[theme.primary, 'transparent']} style={styles.lg} />
+  )
+}
+
+const styles = StyleSheet.create({
+  lg: {
+    position: 'absolute',
+    width: dimensions.fullWidth,
+    height: '100%',
+    top: 0,
+    left: 0
+  }
+})
