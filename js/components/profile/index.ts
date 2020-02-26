e56b5cd3510a8d9772d50973304867089e0f6fed
DavidTalbot <dctalbot@umich.edu>
2020-02-26 01:09:06 -0600

    move show history list into Profile

--- js/components/profile/index.ts
+++ js/components/profile/index.ts
@@ -1,3 +1,2 @@
 export {default as DjBio } from './DjBio'
 export {default as DjCover } from './DjCover'
-export {default as ShowHistory } from './ShowHistory'
--- js/screens/Profile.tsx
+++ js/screens/Profile.tsx
@@ -1,12 +1,26 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, View } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler'
+import React, { useState, useEffect, useContext } from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
 import Screen from '../components/Screen'
 import { spacing } from '../styles/main'
-import { ShowHistory, DjCover, DjBio } from '../components/profile'
+import { DjCover, DjBio } from '../components/profile'
 import { BASE_URL } from '../config'
+import ListItemWrapper from '../components/ListItemWrapper'
+import ThemedText from '../components/ThemedText'
+import ListHeader from '../components/ListHeader'
+import Separator from '../components/Separator'
+import { ThemeContext } from '../styles/theming'
+import LazyPlaceholder from '../components/schedule/LazyPlaceholder'
+
+const renderShowListing = ({ item }) => {
+  return (
+    <ListItemWrapper>
+      <ThemedText>{item.name}</ThemedText>
+    </ListItemWrapper>
+  )
+}
 
 export default function Profile({ route, navigation }) {
+  const { theme } = useContext(ThemeContext)
   const [state, setState] = useState({
     dj_name: '',
     about: '',
@@ -26,25 +40,39 @@ export default function Profile({ route, navigation }) {
 
   return (
     <Screen>
-      <ScrollView overScrollMode={'never'}>
-        <View style={styles.container}>
-          <DjCover
-            dj_name={state.dj_name}
-            website={state.website}
-            public_email={state.public_email}
-            real_name={state.real_name}
-            image_url={state.image_url}
-          />
-          <DjBio about={state.about} />
-        </View>
-        <ShowHistory shows={state.shows} />
-      </ScrollView>
+      <FlatList
+        scrollEnabled
+        data={state.shows}
+        renderItem={renderShowListing}
+        keyExtractor={(item, index) => index.toString()}
+        ListHeaderComponent={
+          <>
+            <View style={styles.mediumPad}>
+              <DjCover
+                dj_name={state.dj_name}
+                website={state.website}
+                public_email={state.public_email}
+                real_name={state.real_name}
+                image_url={state.image_url}
+              />
+              <DjBio about={state.about} />
+            </View>
+            <ListHeader text="Show History" />
+          </>
+        }
+        ItemSeparatorComponent={() => <Separator color={theme.muted} />}
+        ListEmptyComponent={LazyPlaceholder}
+        contentContainerStyle={styles.flatList}
+      />
     </Screen>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  flatList: {
+    flexGrow: 1
+  },
+  mediumPad: {
     padding: spacing.md
   }
 })
