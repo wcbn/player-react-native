9acb24fb7cbd918b65207aa12aea02a99ee8adf5
DavidTalbot <David.Talbot@target.com>
2020-05-25 01:36:31 -0500

    js -> ts

--- src/components/profile/DjBio.tsx
+++ src/components/profile/DjBio.tsx
@@ -1,11 +1,17 @@
+/* eslint-disable react/display-name */
 import React, { useContext } from 'react'
 import { StyleSheet, Image, Linking } from 'react-native'
 import HTML from 'react-native-render-html'
 import { dimensions, spacing } from '../../styles/main'
 import { ThemeContext } from '../../styles/theming'
 import { BASE_URL } from '../../config'
+import ThemedText from '../ThemedText'
 
-export default DjBio = (props) => {
+interface DJBioProps {
+  about: string
+}
+
+export default function DJBio(props: DJBioProps) {
   const { theme } = useContext(ThemeContext)
 
   const styles = StyleSheet.create({
@@ -21,15 +27,16 @@ export default DjBio = (props) => {
     },
   })
 
-  const renderers = {
+  const renderers: HTML.RendererDictionary = {
     img: (htmlAttribs, children, convertedCSSStyles, passProps) => (
       <Image
         style={[
           styles.image,
           {
             height:
-              (parseInt(htmlAttribs.height) * passProps.imagesMaxWidth) /
-              parseInt(htmlAttribs.width),
+              (parseInt(htmlAttribs.height as string) *
+                passProps.imagesMaxWidth) /
+              parseInt(htmlAttribs.width as string),
           },
         ]}
         source={{ uri: `${BASE_URL}${htmlAttribs.src}` }}
@@ -38,15 +45,15 @@ export default DjBio = (props) => {
     ),
   }
 
-  const listsPrefixesRenderers = {
-    ul: (htmlAttribs, children, convertedCSSStyles, passProps) => (
+  const listsPrefixesRenderers: HTML.RendererDictionary = {
+    ul: () => (
       <ThemedText style={styles.listsPrefixesRenderers} color={'secondary'}>
         •
       </ThemedText>
     ),
     ol: (htmlAttribs, children, convertedCSSStyles, passProps) => (
       <ThemedText style={styles.listsPrefixesRenderers} color={'secondary'}>
-        {`${passProps.index + 1})`}
+        {`${passProps.nodeIndex + 1})`}
       </ThemedText>
     ),
   }
