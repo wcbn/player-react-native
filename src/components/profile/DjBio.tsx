6dffbc18b6ba1de8ad602748f6bf94602a69bc95
dctalbot <david.talbot@target.com>
2021-09-04 12:29:04 -0500

    bump render html

--- src/components/profile/DjBio.tsx
+++ src/components/profile/DjBio.tsx
@@ -1,61 +1,42 @@
-/* eslint-disable react/display-name */
 import React, { useContext } from 'react'
-import { StyleSheet, Image } from 'react-native'
-import HTML, { RendererDictionary } from 'react-native-render-html'
-import { dimensions, spacing } from '../../styles/main'
+import { useWindowDimensions } from 'react-native'
+import RenderHtml, {
+  RenderHTMLProps,
+  useInternalRenderer,
+} from 'react-native-render-html'
+import { spacing } from '../../styles/main'
 import { ThemeContext } from '../../styles/theming'
 import { BASE_URL } from '../../config'
-import ThemedText from '../ThemedText'
 
 interface DJBioProps {
   about: string
 }
 
+function ImgRenderer(props: any) {
+  const { Renderer, rendererProps } = useInternalRenderer('img', props)
+
+  const source = {
+    ...rendererProps.source,
+  }
+
+  // folks over at react-native-render-html thought this was a good idea?
+  const dumbPrefix = 'about://'
+  if (source.uri?.startsWith(dumbPrefix)) {
+    source.uri = `${BASE_URL}${source.uri.replace(dumbPrefix, '')}`
+  }
+
+  return (
+    <Renderer
+      {...rendererProps}
+      source={source}
+      style={{ marginTop: spacing.md, marginBottom: 2 }}
+    />
+  )
+}
+
 export default function DJBio(props: DJBioProps) {
   const { theme } = useContext(ThemeContext)
-
-  const styles = StyleSheet.create({
-    listsPrefixesRenderers: {
-      marginRight: spacing.xs,
-    },
-    baseFontStyle: {
-      color: theme.textColor,
-    },
-    image: {
-      marginTop: spacing.md,
-      marginBottom: 2,
-    },
-  })
-
-  const renderers: RendererDictionary = {
-    img: (htmlAttribs, children, convertedCSSStyles, passProps) => (
-      <Image
-        style={[
-          styles.image,
-          {
-            height:
-              (parseInt(htmlAttribs.height as string) * dimensions.fullWidth) /
-              parseInt(htmlAttribs.width as string),
-          },
-        ]}
-        source={{ uri: `${BASE_URL}${htmlAttribs.src}` }}
-        key={htmlAttribs.src}
-      />
-    ),
-  }
-
-  const listsPrefixesRenderers: RendererDictionary = {
-    ul: () => (
-      <ThemedText style={styles.listsPrefixesRenderers} color={'secondary'}>
-        •
-      </ThemedText>
-    ),
-    ol: (htmlAttribs, children, convertedCSSStyles, passProps) => (
-      <ThemedText style={styles.listsPrefixesRenderers} color={'secondary'}>
-        {`${passProps.nodeIndex + 1})`}
-      </ThemedText>
-    ),
-  }
+  const { width } = useWindowDimensions()
 
   // IMPORTANT NOTE : Do NOT use the StyleSheet API to create the styles you're going
   // to feed to tagsStyle and classesStyles. Although it might look like it's working
@@ -63,7 +44,7 @@ export default function DJBio(props: DJBioProps) {
   // to deep check each of your style to properly apply the precedence and priorities
   // of your nested tags' styles.
 
-  const tagsStyles = {
+  const tagsStyles: RenderHTMLProps['tagsStyles'] = {
     figure: {
       marginBottom: spacing.md,
       textAlign: 'center',
@@ -79,18 +60,16 @@ export default function DJBio(props: DJBioProps) {
     },
   }
 
+  if (!props.about) return null
+
   return (
-    <>
-      {!!props.about && (
-        <HTML
-          source={{ html: props.about }}
-          baseFontStyle={styles.baseFontStyle}
-          renderers={renderers}
-          listsPrefixesRenderers={listsPrefixesRenderers}
-          tagsStyles={tagsStyles}
-          computeEmbeddedMaxWidth={() => dimensions.fullWidth}
-        />
-      )}
-    </>
+    <RenderHtml
+      contentWidth={width}
+      source={{ html: props.about }}
+      baseStyle={{ color: theme.textColor }}
+      renderers={{ img: ImgRenderer }}
+      tagsStyles={tagsStyles}
+      computeEmbeddedMaxWidth={() => width}
+    />
   )
 }
